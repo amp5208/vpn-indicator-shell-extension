f28f72a1676269bd44aec9858a650c24393e828c
amp5208 <arnoldpelka@gmail.com>
2019-08-11 22:03:54 -0700

    unworking updates to change icon

--- extension.js
+++ extension.js
@@ -1,17 +1,25 @@
 const St = imports.gi.St;
+const GObject = imports.gi.GObject;
 const Main = imports.ui.main;
 const Lang = imports.lang;
 const Util = imports.misc.util;
 const PanelMenu = imports.ui.panelMenu;
 const Mainloop = imports.mainloop;
 const GLib = imports.gi.GLib;
+const Gio = imports.gi.Gio;
 const Clutter = imports.gi.Clutter;
 
-const VpnIndicator = new Lang.Class({
-    Name: 'VpnIndicator',
-    Extends: PanelMenu.Button,
+const Convenience = Me.imports.convenience;
+const Me = imports.misc.extensionUtils.getCurrentExtension();
+
+const IndicatorName = "VpnIndicator";
+
+const VpnIndicator = GObject.registerClass(
+    class VpnIndicator extends PanelMenu.Button {
 
     _init: function() {
+        super._init(null, IndicatorName);
+
         this.parent(0.0, "VPN Indicator", false);
         this.buttonText = new St.Label({
             text: _("Loading..."),
@@ -19,6 +27,11 @@ const VpnIndicator = new Lang.Class({
         });
 
         this.actor.add_actor(this.buttonText);
+        this._icon = new St.Icon({
+            style_class: 'system-status-icon'
+        });
+        this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/up.svg`);
+        this.actor.add_actor(this._icon);
         this._refresh();
     },
 
@@ -51,19 +64,28 @@ const VpnIndicator = new Lang.Class({
         }
 
         this.buttonText.set_text(text);
+        this._icon = new St.Icon({
+            style_class: 'system-status-icon'
+        });
+        this._icon.gicon = Gio.icon_new_for_string(`${Me.path}/icons/up.svg`);
+        this.actor.add_actor(this._icon);
     }
 });
 
 let twMenu;
 
 function init() {
+    Convenience.initTranslations();
+    let theme = imports.gi.Gtk.IconTheme.get_default();
+    theme.append_search_path(`${extensionMeta.path}/icons`);
 }
 
 function enable() {
     twMenu = new VpnIndicator;
-    Main.panel.addToStatusArea('vpn-indicator', twMenu);
+    Main.panel.addToStatusArea(IndicatorName, twMenu);
 }
 
 function disable() {
     twMenu.destroy();
+    twMenu = null;
 }
